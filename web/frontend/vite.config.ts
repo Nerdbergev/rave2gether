ae0c7778f0b152fc66d4157899f2e323c4afcef8
Fabian Siegel <fabians1999@gmail.com>
2025-04-05 11:08:13 +0200

    feat: use vite proxy to allow easy development of frontend

--- web/frontend/vite.config.ts
+++ web/frontend/vite.config.ts
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true
+      }
+    }
+  }
 })
